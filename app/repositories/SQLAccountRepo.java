c208aa5f91a455c253bce0baf94e031098eecc6b
Rick <rick.van.schijndel@hotmail.com>
2018-06-05 09:23:25 +0200

    Fix minor DB issues

--- app/repositories/SQLAccountRepo.java
+++ app/repositories/SQLAccountRepo.java
@@ -3,9 +3,7 @@ package repositories;
 import domains.Account;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import java.util.List;
 
 public class SQLAccountRepo implements IAccountRepo {
@@ -121,7 +119,7 @@ public class SQLAccountRepo implements IAccountRepo {
             // Copy over a few properties.
             existingAcc.setEmail(newAccProperties.getEmail());
             existingAcc.setPassword(newAccProperties.getPassword());
-            
+
             // Finally commit changes.
             et.commit();
 
